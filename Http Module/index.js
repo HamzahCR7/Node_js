821c0398f6bb34dd5042580e63e6d4ce1f00cda9
hamzacr7 <hamzahaslam523@gmail.com>
2023-06-18 14:43:37 +0530

    adding the error type for not found url

--- Http Module/index.js
+++ Http Module/index.js
@@ -3,12 +3,13 @@ const http = require('http');//import the core http module ;
 const server = http.createServer((req, res) => {
   const { url, method, headers } = req;
   console.log(`your url is ${url} and method is ${method}`);
-  res.writeHead(200, { "content-type": "text/plain" });
+  res.writeHead(200, { "content-type": "text/html" });
   if (url == "/") {
     res.write("Home");
   } else if (url == "/about") {
     res.write("About");
   } else {
+    res.writeHead('404');
     res.write("Page not found");
   }
   res.end(); //creating a server
